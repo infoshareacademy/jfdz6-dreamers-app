493ad639e3215601536cb79815fe1a994ee67395
Damian Wisniewski <wisniad@gmail.com>
2017-12-03 13:50:46 +0100

    Test Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
+import {
+    Navbar,
+    Nav,
+    NavItem
+} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import {
+    LinkContainer
+} from 'react-router-bootstrap'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <Router>
+                        <div>
+                            <Navbar>
+                                <Navbar.Header>
+                                    <Navbar.Brand>
+                                        <a href="/">Dreamers</a>
+                                    </Navbar.Brand>
+                                </Navbar.Header>
+                                <Nav>
+                                    <LinkContainer exact to="/">
+                                        <NavItem>Home</NavItem>
+                                    </LinkContainer>
+
+                                    <LinkContainer to="/about">
+                                        <NavItem>About</NavItem>
+                                    </LinkContainer>
+
+                                </Nav>
+                            </Navbar>
+
+                            <hr/>
+
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/about" component={About}/>
+
+                        </div>
+                    </Router>
+
+                </header>
+            </div>
+        );
+    }
 }
+
+const Home = () => (
+    <div>
+        <h2>Home AWESO</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+        <h1>Mwahahaha</h1>
+        <p>I know props</p>
+
+    </div>
+)
 
 export default App;
