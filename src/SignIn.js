a0fdc3e61ac2217b13a9af5d6a8ed009b88b5b69
Andrzej Puzynski <andrzejpuzynski@tlen.pl>
2018-01-26 22:00:17 +0100

    Add RWD for SignIn

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,30 +1,50 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import AuthForm from './AuthForm';
-import { signIn } from './state/auth'
+import {signIn} from './state/auth'
+import {
+    Grid,
+    Row,
+    Col,
+    Image,
+    Well,
+} from 'react-bootstrap'
+
+import './SignIn.css'
 
 class SignIn extends Component {
 
-  render() {
-    return (
-      <AuthForm
-        label={'Zaloguj'}
-        error={this.props.auth.error}
-        handleSubmit={this.props.signIn}
-      />
-    )
-  }
+    render() {
+        return (
+            <Grid>
+                <Row className="show-grid">
+                    <h1>Logowanie</h1>
+                </Row>
+                <Row className="show-grid">
+                    <Col sm={8} smOffset={2} md={6} mdOffset={3}>
+                        <div className="SignIn_box SignIn_AuthForm ">
+                            <AuthForm
+                                label={'Zaloguj'}
+                                error={this.props.auth.error}
+                                handleSubmit={this.props.signIn}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
 }
 
 const mapStateToProps = state => ({
-  auth: state.auth
+    auth: state.auth
 })
 
 const mapDispatchToProps = dispatch => ({
-  signIn: (email, password) => dispatch(signIn(email, password))
+    signIn: (email, password) => dispatch(signIn(email, password))
 })
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(SignIn)
