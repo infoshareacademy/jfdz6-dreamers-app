2820c6feeb74a1eebd876aecfb2c8be9611cf92a
Andrzej Puzynski <andrzejpuzynski@tlen.pl>
2018-01-27 20:14:05 +0100

    Test connections

--- src/MyEvents.js
+++ src/MyEvents.js
@@ -1,6 +1,9 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
-import {getMyEvents} from './state/myevents'
+import {getEvents} from './state/myevents'
+import {saveEvent} from './state/myevents'
+import firebase from 'firebase'
+
 import {
     Grid,
     Row,
@@ -9,9 +12,9 @@ import {
 } from 'react-bootstrap'
 
 import './Events.css'
-
-
-class MyEvents extends Component {
+import {currentUser} from './state/auth'
+import myevents from "./state/myevents";
+class Events extends React.Component {
     state = {
         events: [],
         deleting: false,
@@ -19,54 +22,120 @@ class MyEvents extends Component {
     }
 
     componentDidMount() {
-        this.props.getMyEvents()
+        this.props.getEvents()
+        this.props.currentUser()
     }
 
-    render() {
+/*
+    componentWillReceiveProps(nextProps) {
+        let data
+        let savedEvents = []
+
+        //console.log(data);
+
+        // user uid HnsrGOy2TuOmU3bdWgkxlICGqlu2
+
+        var promise1 = new Promise(function(resolve, reject) {
+            let dbRefObject = firebase.database().ref().child('users')
+
+            dbRefObject.on('value', snap => {
+
+                data = Object.values(snap.val())
+                console.log(data)
+                resolve(data);
+            } )
+        });
+
+        promise1.then(function(value) {
+            console.log(data[0]);
+            data[0]['events'].map( event => savedEvents.push(event))
+            // expected output: "Success!"
+            console.log(savedEvents)
+        })
+
+
+        const {eventofaday} = nextProps; //destructuring
+
+
+        const myEvents = (myevents.data)
+            .filter(
+                event =>
+                    (event.id.includes(savedEvents))
+            );
+
+
+    }
+
+
+  */  render() {
         return (
             <Grid>
                 <Row className="show-grid">
-                    <h1>my events</h1>
+                    <h1>Wydarzenia</h1>
                 </Row>
 
                 <Row className="show-grid">
-                    <Col sm={8} smOffset={2} md={6} mdOffset={3}>
+                    {
+                        this.state.error && <p>{this.state.error.message}</p>
+                    }
 
-                        <Row className="show-grid">
-                            <Col xs={12}>
-                                {
-                                    (this.props.data || [])
-                                        .map(
-                                            event => (
-                                                <Col xs={12} sm={6} md={4} lg={3}>
-                                                    <p
-                                                        key={event.id}
-                                                        className="Events_box">
-                                                        <div className="Events_title">
-                                                            <h4>{event.name}</h4>
-                                                        </div>
-                                                        <div className="Events_image">
-                                                            <a href={checkIfUrlExist(event)}>
-                                                                <Image src={checkIfAttachmentExist(event)} responsive
-                                                                       className="Events_image_link"/>
-                                                            </a>
-                                                        </div>
-                                                        <div className="Events_time">
-                                                            <p>{`Data: ${getDateNode(event).slice(0, 10)} ${getDateNode(event).slice(11, -3)}`}</p>
-                                                        </div>
-                                                    </p>
-                                                </Col>
-                                            )
-                                        )
-                                }
-                            </Col>
-                        </Row>
+                    {
+                        this.props.events.getting && <p>Getting events...</p>
+                    }
+                </Row>
+                <Row className="show-grid">
+                    <Col xs={12}>
+                        {                            console.log('THIS IS DATA',this.props.auth.data)
+                        }
+                        {
+                            (this.props.events.data || [])
+                                .filter(
+                                    event => (
+
+
+
+
+                                        saveEvents.includes(event.id))
+                                )
+                                .map(
+                                    event => (
+                                        <Col xs={12} sm={6} md={4} lg={3}>
+                                            <p
+                                                key={event.id}
+                                                className="Events_box">
+                                                <div className="Events_title">
+                                                    <h4>{event.name}</h4>
+                                                </div>
+                                                <div className="Events_image">
+                                                    <a href={checkIfUrlExist(event)}>
+                                                        <Image src={checkIfAttachmentExist(event)} responsive
+                                                               className="Events_image_link"/>
+                                                    </a>
+                                                </div>
+                                                <div className="Events_time">
+                                                    <p>{`Data: ${getDateNode(event).slice(0, 10)} ${getDateNode(event).slice(11, -3)}`}</p>
+                                                </div>
+                                            </p>
+                                        </Col>
+                                    )
+                                )
+                        }
                     </Col>
                 </Row>
             </Grid>
         )
     }
 }
+
+const mapStateToProps = state => ({
+    events: state.events,
+    auth: state.auth
+})
+
+const mapDispatchToProps = dispatch => ({
+    getEvents: () => dispatch(getEvents()),
+    currentUser: () => dispatch(currentUser())
+})
 
 
 function checkIfAttachmentExist(event) {
@@ -93,15 +162,38 @@ function getDateNode(event) {
 }
 
 
-const mapStateToProps = state => ({
-    data: state.data
-})
+function saveEvents() {
+    let data
+    let savedEvents = []
 
-const mapDispatchToProps = dispatch => ({
-    getMyEvents: () => dispatch(getMyEvents())
-})
+    //console.log(data);
+
+    // user uid HnsrGOy2TuOmU3bdWgkxlICGqlu2
+
+    var promise1 = new Promise(function(resolve, reject) {
+        let dbRefObject = firebase.database().ref().child('users')
+
+        dbRefObject.on('value', snap => {
+
+            data = Object.values(snap.val())
+     //       console.log(data)
+            resolve(data);
+        } )
+    });
+
+    promise1.then(function(value) {
+      //  console.log(data[0]);
+        data[0]['events'].map( event => savedEvents.push(event))
+        // expected output: "Success!"
+       // console.log(savedEvents)
+    })
+
+
+
+    return savedEvents;
+}
 
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(MyEvents)
+)(Events)
--- src/state/myevents.js
+++ src/state/myevents.js
@@ -1,43 +1,21 @@
 import firebase from 'firebase'
 
-const GET_BEGIN = 'myevents/GET_BEGIN'
-const GET_SUCCESS = 'myevents/GET_SUCCESS'
-const GET_FAIL = 'myevents/GET_FAIL'
+
+const GET_BEGIN = 'events/GET_BEGIN'
+const GET_SUCCESS = 'events/GET_SUCCESS'
+const GET_FAIL = 'events/GET_FAIL'
+
+export const getEvents = ( dateFrom, dateTo ) => dispatch => {
+    // let urltest = 'http://planer.info.pl/api/rest/events.json?start_date='+day+'&limit=12';
+    //   console.log('Od : ',dateFrom.toJSON().slice(0,10));
+    //   console.log('Do : ', dateTo.toJSON().slice(0,10));
 
 
-const initialState = {
-    data: null,
-    getting: false,
-    error: null,
-}
+    //console.log('Od : ',dateFrom.toJSON().slice(0,10));
+    //console.log('Do : ', dateTo.toJSON().slice(0,10));
 
 
 
-export const getMyEvents = () => dispatch => {
-//saving data
-/*
-    let ref = firebase.database().ref()
-    var usersRef = ref.child("users");
-    usersRef.set({
-        damian: {
-            events: [
-                "44697",
-                "45026"
-            ]},
-        andrzej: {
-                events: [
-                    "44697",
-                    "45026"
-                ]},
-        przemek: {
-            events: [
-                "44397",
-                "44226"
-            ]}
-
-            });
-*/
-    //console.log(firebase.auth())
 
     let data
     let savedEvents = []
@@ -67,20 +45,28 @@ export const getMyEvents = () => dispatch => {
 
 
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'http://planer.info.pl/api/rest/events.json?limit=100';
-
+        targetUrl = 'http://planer.info.pl/api/rest/events.json?&limit=250';
+    //   console.log('link', targetUrl)
     dispatch({ type: GET_BEGIN })
+
+
     return fetch(
         proxyUrl + targetUrl
     ).then(
         response => response.json()
-    /* ).then(
-        toFilter => toFilter.filter( f => savedEvents.includes(f)) */
     ).then(
-        data => dispatch({ type: GET_SUCCESS, data })
+        data => dispatch({ type: GET_SUCCESS, data,dateFrom,dateTo })
     ).catch(
         error => dispatch({ type: GET_FAIL, error })
     )
+}
+
+
+
+const initialState = {
+    data: null,
+    getting: false,
+    error: null,
 }
 
 export default (state = initialState, action = {}) => {
@@ -97,6 +83,8 @@ export default (state = initialState, action = {}) => {
                 ...state,
                 data: action.data,
                 getting: false,
+                dateFrom: action.dateFrom,
+                dateTo: action.dateTo
             }
         case GET_FAIL:
             return {
